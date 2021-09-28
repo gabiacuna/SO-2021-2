64e7ec4459fcfc8fcf4f60580784ad93c0ac8d51
Kaunaz <tomaxmg@gmail.com>
2021-09-28 10:39:42 -0300

    Comentado buscar

---
+++ buscar.c
@@ -0,0 +1,74 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+
+
+int main(){
+    // PATH a modificar para revisar si es archivo existe.
+    char path_n3[] = "./Laberinto/x/y/z/frase.txt";
+
+    
+    char frase_1[22];
+    char frase_2[40];
+    char frase_3[40];
+
+    // Ingresamos a todos los nodos externos del arbol.
+    for (int i = 1; i < 4; i++)
+    {
+        path_n3[12] = i + '0';
+        for (int j = 1; j < 4; j++)
+        {
+            path_n3[14] = j + '0';
+            for (int k = 1; k < 4; k++)
+            {
+                path_n3[16] = k + '0';
+                // Revisamos si el nodo externo tiene alguno de los 3 archivos
+                if( access( path_n3, R_OK ) == 0 ) {
+                    // Si existe, leemos el archivo y extraemos el string.
+                    FILE *fp = fopen(path_n3, "r");
+                    char line_temp[55];
+                    fgets(line_temp, 50, fp);
+                    fclose(fp);
+                    // Analizamos el primero char de cada string, para ver si es la frase 1,2 o 3, para guardarla en su correspondiente variable.
+                    if (line_temp[0] == '1')
+                    {
+                        strcpy(frase_1, line_temp);
+                    }else if (line_temp[0] == '2')
+                    {
+                        strcpy(frase_2, line_temp);
+                    }else if (line_temp[0] == '3')
+                    {
+                        strcpy(frase_3, line_temp);
+                    }
+
+                } 
+            }
+            
+        }
+        
+    }
+    
+    // Movemos el puntero header del string para saltarse el "1, "/
+    char* f1;
+    char* f2;
+    char* f3;
+
+    f1 = frase_1;
+    f2 = frase_2;
+    f3 = frase_3;
+
+    for (int i = 0; i < 3; i++)
+    {
+        f1++;
+        f2++;
+        f3++;
+    }
+    
+    // Imprmimos las 3 frases en conjunto y ordenadas
+    printf("%s%s%s\n", f1, f2, f3);
+    return 0;
+}
---
+++ laberinto.c
@@ -0,0 +1,111 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+
+struct stat st = {0};
+
+int main (){
+    // Revision de existencia de carpeta Laberinto, si existe, el programa se detiene.
+    if (stat("./Laberinto", &st) != -1) {
+
+        printf("Ya existe un directorio llamado Laberinto, por favor eliminelo.\n");
+        exit(EXIT_FAILURE);
+
+    }
+    //  Creacion de carpeta principal "Laberinto"
+    mkdir("./Laberinto", 0777);
+
+    // Creamos 
+    char path_n1[] = "./Laberinto/x";
+    char path_n2[25] = "./Laberinto/x/y";
+    char path_n3[30] = "./Laberinto/x/y/z";
+
+    //  Creamos una variable de tiempo para que se generen distintos numeros random mas adelante
+    time_t t;
+    srand((unsigned) time(&t)); // Randomizamos la semilla de la ejecucion.
+
+    // Inicializamos 3 numeros aleatorio que van del 0-26.
+    int a,b,c;
+    a = rand()%27;
+    b = rand()%27;
+    c = rand()%27;
+
+    // Nos aseguramos de que los 3 numeros sean distintos. 
+    while (a == b || a == c || b == c)
+    {
+        a = rand()%27;
+        b = rand()%27;
+        c = rand()%27;
+    }
+    
+    int counter = 0;
+
+    // Eleguimos al azar las posiciones de las frases
+    int* pos = (int*)calloc(30, sizeof(int));
+    pos[a] = 1;
+    pos[b] = 2;
+    pos[c] = 3;  
+
+
+    for (int i = 1; i < 4; i++)
+    {
+        path_n1[12] = i + '0';
+        path_n2[12] = i + '0';
+        path_n3[12] = i + '0';
+
+        mkdir(path_n1, 0777);
+        
+        for (int j = 1; j < 4; j++)
+        {
+            path_n2[14] = j + '0';
+            path_n3[14] = j + '0';
+
+            mkdir(path_n2, 0777);
+
+            for (int k = 1; k < 4; k++)
+            {
+                path_n3[16] = k + '0';
+                mkdir(path_n3, 0777);
+
+                if (pos[counter] == 1){
+                    char path_f1[30];
+                    strcpy(path_f1, path_n3);
+                    strcat(path_f1, "/frase.txt");
+
+                    FILE *fp1;
+                    fp1 = fopen(path_f1, "w");
+                    fprintf(fp1, "1, Los hurones del ");
+                    fclose(fp1);  
+                }
+                else if (pos[counter] == 2){
+                    char path_f2[30];
+                    strcpy(path_f2, path_n3);
+                    strcat(path_f2, "/frase.txt");
+                    
+                    FILE *fp2;
+                    fp2 = fopen(path_f2, "w");
+                    fprintf(fp2, "2, profesor de sistemas operativos son ");
+                    fclose(fp2);
+                } 
+                else if (pos[counter] == 3){
+                    char path_f3[30];
+                    strcpy(path_f3, path_n3);
+                    strcat(path_f3, "/frase.txt");
+                    
+                    FILE *fp3;
+                    fp3 = fopen(path_f3, "w");
+                    fprintf(fp3, "3, realmente carpinchos disfrazados.");
+                    fclose(fp3);
+                }   
+                counter += 1; 
+            }          
+        }
+    }
+
+    free(pos);
+
+    return 0;
+}
